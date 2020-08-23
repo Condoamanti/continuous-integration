8fba1794a5302c9389eac42a55b3a1f851bc3da1
Condoamanti <Condoamanti@domain.local>
2020-08-23 01:35:17 -0500

    updating configuration for golangCI.groovy

--- vars/golangCI.groovy
+++ vars/golangCI.groovy
@@ -1,48 +1,50 @@
 import local.domain.*
 
-podTemplate(
-  cloud: 'kubernetes',
-  namespace: 'jenkins',
-  activeDeadlineSeconds: '300',
-  showRawYaml: 'false',
-  containers: [
-  containerTemplate(
-    name: 'centos',
-    image: 'centos:centos7',
-    ttyEnabled: true,
-    privileged: true,
-    command: '/usr/sbin/init'
-  )
-]) {
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    
+    podTemplate(
+    cloud: 'kubernetes',
+    namespace: 'jenkins',
+    activeDeadlineSeconds: '300',
+    showRawYaml: 'false',
+    containers: [
+        containerTemplate(
+            name: 'centos',
+            image: 'centos:centos7',
+            ttyEnabled: true,
+            privileged: true,
+            command: '/usr/sbin/init'
+        )
+    ]) {
 
 
-    try {
-        node(POD_LABEL) {
-        def call(body) {
-            def config = [:]
-            body.resolveStrategy = Closure.DELEGATE_FIRST
-            body.delegate = config
-            body()
+        try {
+            node(POD_LABEL) {
 
-            // Ensure imported classes are null
-            Docker docker = null
 
-            stage("Clean Workspace") {
-                cleanWs()
+                // Ensure imported classes are null
+                Docker docker = null
+
+                stage("Clean Workspace") {
+                    cleanWs()
+                }
+
+                stage("Create Dependencies") {
+                    docker = new Docker(this)
+                }
+
+                stage("Run Docker Class") {
+                    docker.test("${config.message}")
+                }
             }
-
-            stage("Create Dependencies") {
-                docker = new Docker(this)
-            }
-
-            stage("Run Docker Class") {
-                docker.test("${config.message}")
-            }
+        } catch (e) {
+            echo "Exception: ${e}"
+            currentBuild.result = 'FAILURE'
+        } finally {
         }
-    }
-    } catch (e) {
-        echo "Exception: ${e}"
-        currentBuild.result = 'FAILURE'
-    } finally {
     }
 }
