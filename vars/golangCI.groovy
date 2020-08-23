9730bfa6b01f3497da2bad2373a179f1c2d82982
Condoamanti <Condoamanti@domain.local>
2020-08-23 09:25:20 -0500

    updating configuration for golangCI.groovy

--- vars/golangCI.groovy
+++ vars/golangCI.groovy
@@ -5,12 +5,15 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-    
+
+    // podTemplate configuration for the container running the below stages
     podTemplate(
     cloud: 'kubernetes',
     namespace: 'jenkins',
     activeDeadlineSeconds: '300',
     showRawYaml: 'false',
+    runAsUser: 0,
+    runAsGroup: 1000,
     containers: [
         containerTemplate(
             name: 'centos',
@@ -21,30 +24,102 @@ def call(body) {
         )
     ]) {
 
-
         try {
             node(POD_LABEL) {
-
-
+/*
                 // Ensure imported classes are null
                 Docker docker = null
 
                 stage("Clean Workspace") {
                     cleanWs()
-                }
+                } // stage end
 
                 stage("Create Dependencies") {
                     docker = new Docker(this)
-                }
+                } // stage end
 
                 stage("Run Docker Class") {
                     docker.test("${config.message}")
-                }
-            }
+                } // stage end
+*/                
+                stage("Get Golang Project") {
+                    git url: 'https://github.com/Condoamanti/programming.git'
+                } // stage end
+
+                container('centos') {
+                    stage('Remove Default CentOS7 Repositories') {
+                    sh """
+                    whoami
+                    mkdir /var/run/docker.sock
+                    chown root:root /var/run/docker.sock
+                    rm -rf /etc/yum.repos.d/*
+                    """
+                    } // stage end
+
+                    stage('Setup Artifactory Repository') {
+                    sh """
+                    echo \"# Artifactory Repository
+        [artifactory-centos-7-os]
+        name=artifactory-centos-7-os
+        baseurl=http://admin:AP72goB1ugbhNixgk4oZQD1JSMK@k8snode1dc1.jittersolutions.com:32382/artifactory/rpm/7/os/x86_64
+        enabled=1
+        gpgcheck=0
+
+        [artifactory-centos-7-extras]
+        name=artifactory-centos-7-extras
+        baseurl=http://admin:AP72goB1ugbhNixgk4oZQD1JSMK@k8snode1dc1.jittersolutions.com:32382/artifactory/rpm/7/extras/x86_64
+        enabled=1
+        gpgcheck=0
+
+        [artifactory-centos-7-fasttrack]
+        name=artifactory-centos-7-fasttrack
+        baseurl=http://admin:AP72goB1ugbhNixgk4oZQD1JSMK@k8snode1dc1.jittersolutions.com:32382/artifactory/rpm/7/fasttrack/x86_64
+        enabled=1
+        gpgcheck=0
+
+        [artifactory-centos-7-updates]
+        name=artifactory-centos-7-updates
+        baseurl=http://admin:AP72goB1ugbhNixgk4oZQD1JSMK@k8snode1dc1.jittersolutions.com:32382/artifactory/rpm/7/updates/x86_64
+        enabled=1
+        gpgcheck=0
+
+        [artifactory-centos-7-cr]
+        name=artifactory-centos-7-cr
+        baseurl=http://admin:AP72goB1ugbhNixgk4oZQD1JSMK@k8snode1dc1.jittersolutions.com:32382/artifactory/rpm/7/cr/x86_64
+        enabled=1
+        gpgcheck=0\" >> /etc/yum.repos.d/artifactory.repo
+                    """
+                    } // stage end
+
+                    stage('Download Docker Dependencies') {
+                    sh """
+                    yum update --assumeyes --quiet
+                    yum install --assumeyes --quiet container-selinux
+                    yum install --assumeyes --quiet yum-utils
+                    yum clean all
+                    """
+                    } // stage end
+
+                    stage('Download Docker') {
+                    sh """
+                    yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+                    yum install --assumeyes --quiet docker-ce
+                    yum clean all
+                    systemctl start docker
+                    """
+                    } // stage end
+
+                    stage('Build Docker Image') {
+                    sh """
+                    docker build -t condoamanti/dockergo ./go/web
+                    ls ./go/web
+                    """
+                    } // stage end
+                } // container end
+            } // node end
         } catch (e) {
             echo "Exception: ${e}"
             currentBuild.result = 'FAILURE'
-        } finally {
         }
-    }
-}
+    } // podTemplate end
+} // call(body) end
