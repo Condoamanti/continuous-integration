0624db7b2fb2ff7bd358dd6827424b21dd9476c6
Condoamanti <Condoamanti@domain.local>
2020-08-25 08:17:52 -0500

    updating DockerCI.groovy

--- vars/DockerCI.groovy
+++ vars/DockerCI.groovy
@@ -21,7 +21,7 @@ def call(body) {
             }
 
             stage("Create Dockerfile") {
-                writeFile(file: "Dockerfile", text: "FROM ${config.imageName}:${config.imageTag}", encoding: "UTF-8")
+                appendFile(file: "Dockerfile", text: "FROM ${config.imageName}:${config.imageTag}", encoding: "UTF-8")
                 sh """
                 ls -lah
                 cat ./Dockerfile
