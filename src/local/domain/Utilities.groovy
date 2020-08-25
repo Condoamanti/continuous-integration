4a07d98aaf7c46d504b00c624114d602caaee1e6
Condoamanti <Condoamanti@domain.local>
2020-08-25 08:57:38 -0500

    updating DockerCI.groovy and updating Utilities.groovy

--- src/local/domain/Utilities.groovy
+++ src/local/domain/Utilities.groovy
@@ -9,10 +9,10 @@ class Utilities {
 
     // Method appendFile which appends text to an existing file
     def appendFile(String fileName, String line, String encoding = "UTF-8") {
-        script.println new File('input.txt').exists()
-        if (File("${fileName}").exists() == false) {
+        
+        if (script.println new File('input.txt').exists() == false) {
             script.println("HERE1")
-            script.writeFile(file: "${fileName}", text: "${line}")
+            script.appendFile(file: "${fileName}", text: "${line}")
         } else {
             script.println("HERE2")
             script.appendFile(file: "${fileName}", text: "\n${line}")
