8e5ce08bc64a4596de10d32b010b4016fc6bdfce
Condoamanti <Condoamanti@domain.local>
2020-09-08 22:56:29 -0500

    removing Utilities.groovy

--- src/local/domain/Utilities.groovy
+++
@@ -1,9 +0,0 @@
-package local.domain
-class Utilities {
-    def script
-
-    // Constructor with the instance of the script invoking this specific class
-    Utilities(script) {
-        this.script = script
-    }
-}
