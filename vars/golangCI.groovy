2311e1a9ece776650c1bd51a4134f0028dff5394
Condoamanti <Condoamanti@domain.local>
2020-09-15 21:28:44 -0500

    updating golangCI.groovy

--- vars/golangCI.groovy
+++ vars/golangCI.groovy
@@ -42,7 +42,7 @@ def call(body) {
                     
                     stage ("Build Go Project") {
                         //go.build("${config.projectPath}")
-                        sh "cd ${config.projectPath}"
+                        sh "cd \$GOPATH/src/${config.projectPath}"
                     } //stage end
 
                     stage ("Run Go Project") {
