64f1621072a61d9f3a9e2433da6a7d18b5146346
Condoamanti <Condoamanti@domain.local>
2020-09-20 04:13:11 -0500

    updating golang

--- vars/golangCI.groovy
+++ vars/golangCI.groovy
@@ -63,10 +63,9 @@ def call(body) {
                         } // switch end
                         println("credentialsId: ${credentialsId}")
                         withCredentials([usernamePassword(credentialsId: "${credentialsId}", usernameVariable: 'packageRepositoryUsername', passwordVariable: 'packageRepositoryPassword')]) {
-                            go.set("${config.projectPath}")
+                            dir("/go/src/${projectPath}")
                             sh "ls"
                             sh "pwd"
-                            dir("/go/src/${projectPath}")
                             jfrog.publish("${packageRepositoryName}", "${BUILD_NUMBER}", "${packageRepositoryUrl}", "${packageRepositoryUsername}", "${packageRepositoryPassword}")
                         }
                     } // stage end
